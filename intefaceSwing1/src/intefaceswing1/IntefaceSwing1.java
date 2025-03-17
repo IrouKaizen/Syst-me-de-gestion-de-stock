75f50b697d8d1f4f6910e931e2f0e94584b6c379
irouKaizen <122926735+IrouKaizen@users.noreply.github.com>
2025-03-17 17:17:39 +0000

    Add files via upload

---
+++ intefaceSwing1/src/intefaceswing1/IntefaceSwing1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intefaceswing1;
+
+/**
+ *
+ * @author Israel
+ */
+public class IntefaceSwing1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Calcul2().setVisible(true);
+    }
+    
+}
---
+++ intefaceSwing1/src/intefaceswing1/NumerateurInvalidException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intefaceswing1;
+
+/**
+ *
+ * @author Israel
+ */
+public class NumerateurInvalidException extends Exception{
+
+    public NumerateurInvalidException(String message) {
+        super(message);
+    }
+    
+}
